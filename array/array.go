f1814d4e725d2d01db5815feb7a93f940bfdefee
eric <eric.zsp@hotmail.com>
2021-03-08 22:58:41 +0800

    Reverse

--- array/array.go
+++ array/array.go
@@ -96,3 +96,22 @@ func RemoveDuplicateInt64(arr []int64) (newArr []int64) {
 	}
 	return
 }
+
+func ReverseStr(l []string)  {
+    for i:=0; i < int(len(l)/2) ;i++{
+        li := len(l) - i -1
+        l[i],l[li] = l[li],l[i]
+    }
+}
+func ReverseInt(l []int)  {
+    for i:=0; i < int(len(l)/2) ;i++{
+        li := len(l) - i -1
+        l[i],l[li] = l[li],l[i]
+    }
+}
+func ReverseInt64(l []int64)  {
+    for i:=0; i < int(len(l)/2) ;i++{
+        li := len(l) - i -1
+        l[i],l[li] = l[li],l[i]
+    }
+}
