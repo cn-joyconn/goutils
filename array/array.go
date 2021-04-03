9bdc94c14651b299e1fed819b490d247187d31b0
eric <eric.zsp@hotmail.com>
2021-04-03 17:39:36 +0800

    array contain

--- array/array.go
+++ array/array.go
@@ -1,6 +1,8 @@
 package array
 
 import (
+	"errors"
+	"reflect"
 	"strconv"
 	"strings"
 )
@@ -114,4 +116,23 @@ func ReverseInt64(l []int64)  {
         li := len(l) - i -1
         l[i],l[li] = l[li],l[i]
     }
+}
+
+
+func Contain(obj interface{}, target interface{}) (bool, error) {
+	targetValue := reflect.ValueOf(target)
+	switch reflect.TypeOf(target).Kind() {
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < targetValue.Len(); i++ {
+			if targetValue.Index(i).Interface() == obj {
+				return true, nil
+			}
+		}
+	case reflect.Map:
+		if targetValue.MapIndex(reflect.ValueOf(obj)).IsValid() {
+			return true, nil
+		}
+	}
+ 
+	return false, errors.New("not in array")
 }
