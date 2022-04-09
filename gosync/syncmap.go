7a233d5147384e502df5a071d70367d4721fc2a0
eric <eric.zsp@hotmail.com>
2022-04-09 22:34:18 +0800

    GetOrDefault

--- gosync/syncmap.go
+++ gosync/syncmap.go
@@ -25,6 +25,16 @@ func (sm *SyncMap[K, V]) Get(k K) (V, bool) {
 	v, ok := sm.source[k]
 	return v, ok
 }
+
+func (sm *SyncMap[K, V]) GetOrDefault(k K, defaultv V) V {
+	sm.mu.RLock()
+	defer sm.mu.RUnlock()
+	v, ok := sm.source[k]
+	if !ok {
+		v = defaultv
+	}
+	return v
+}
 func (sm *SyncMap[K, V]) Remove(k K) {
 	sm.mu.Lock()
 	defer sm.mu.Unlock()
